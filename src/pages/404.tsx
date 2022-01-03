38707a834e4822f7fffcb7595814843a3b3e712f
Adam Coogan <dr.adam.coogan@gmail.com>
2022-01-03 17:08:24 -0500

    add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const Error404Page = () => (
+  <Layout>
+    <div>
+      <h2>404</h2>
+      <p>Uh oh! Looks like the page you're looking for does not exist.</p>
+    </div>
+  </Layout>
+);
+
+export default Error404Page;
