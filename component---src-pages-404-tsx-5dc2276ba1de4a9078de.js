c0db8a250133f1b8f851de3d8aa27958793ab89a
Adam Coogan <dr.adam.coogan@gmail.com>
2022-01-05 20:54:00 -0500

    Updates

---
+++ component---src-pages-404-tsx-5dc2276ba1de4a9078de.js
@@ -0,0 +1,2 @@
+(self.webpackChunkadam_coogan_github_io=self.webpackChunkadam_coogan_github_io||[]).push([[218],{938:function(e,n,t){"use strict";t.r(n);var l=t(7294),o=t(1676);n.default=function(){return l.createElement(o.Z,null,l.createElement("div",null,l.createElement("h2",null,"404"),l.createElement("p",null,"Uh oh! Looks like the page you're looking for does not exist.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-5dc2276ba1de4a9078de.js.map
